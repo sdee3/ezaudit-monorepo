967f444b1c60647d5370568f080c2e4681ee0fee
Stefan <stefan.dokic@saboit.de>
2021-12-19 13:56:17 +0100

    EA-35 Cypress tests

--- client/cypress/integration/pages/homepage.ts
+++ client/cypress/integration/pages/homepage.ts
@@ -1,4 +1,16 @@
-it('loads homepage', () => {
+beforeEach('load homepage', () => {
   cy.visit('/')
   cy.dataCy('submitAuditBtn').should('be.visible')
+  cy.dataCy('submitAuditInput').should('be.visible')
+})
+
+it('sees a disabled submit button if the domain value is invalid', () => {
+  cy.dataCy('submitAuditInput').type('google')
+  cy.dataCy('submitAuditBtn').should('be.disabled')
+})
+
+it('sees an enabled submit button if the domain input value is valid', () => {
+  cy.dataCy('submitAuditInput').clear()
+  cy.dataCy('submitAuditInput').type('google.com')
+  cy.dataCy('submitAuditBtn').should('not.be.disabled')
 })

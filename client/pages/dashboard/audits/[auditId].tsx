ef476b14f94dab1c7c1fc824111d9ebb080e3bac
Stefan <stefan.dokic@saboit.de>
2022-01-24 20:23:13 +0100

    EA-43 Breadcrumbs on Selected Audit page

--- client/pages/dashboard/audits/[auditId].tsx
+++ client/pages/dashboard/audits/[auditId].tsx
@@ -1,16 +1,29 @@
 import { Container, Heading, Text } from '@chakra-ui/react'
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 
+import { Breadcrumbs } from '../../../components'
 import {
   AuditResult,
   AuditResultFromAPI,
   AuditResultParsed,
+  BreadcrumbLink,
 } from '../../../models'
-import { fetchFromApi } from '../../../utils'
+import { fetchFromApi, ROUTES } from '../../../utils'
 
 interface PageProps {
   audit: AuditResultParsed
 }
+
+const BREADCRUMB_LINKS: BreadcrumbLink[] = [
+  {
+    route: ROUTES.dashboard,
+    name: 'Dashboard',
+  },
+  {
+    route: ROUTES.userAudits,
+    name: 'Audits',
+  },
+]
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
@@ -35,24 +48,29 @@ export const getServerSideProps: GetServerSideProps = async (
 
 const AuditByIdOverview = ({ audit }: PageProps) => {
   return (
-    <Container maxW="container.xl">
-      <Heading mb={4}>
-        Audit of{' '}
-        <a href={audit.domain} target="_blank" rel="noopener noreferrer">
-          {audit.domain}
-        </a>
-      </Heading>
-      <Text mb={4}>
-        <strong>Date of audit:</strong>{' '}
-        {new Date(audit.date_of_request).toString()}
-      </Text>
-      <Text>Accessibility: {audit.audit_result.accessibility.score * 100}</Text>
-      <Text>
-        Best Practices: {audit.audit_result['best-practices'].score * 100}
-      </Text>
-      <Text>Performance: {audit.audit_result.performance.score * 100}</Text>
-      <Text>SEO: {audit.audit_result.seo.score * 100}</Text>
-    </Container>
+    <>
+      <Breadcrumbs links={BREADCRUMB_LINKS} />
+      <Container maxW="container.xl">
+        <Heading mb={4}>
+          Audit of{' '}
+          <a href={audit.domain} target="_blank" rel="noopener noreferrer">
+            {audit.domain}
+          </a>
+        </Heading>
+        <Text mb={4}>
+          <strong>Date of audit:</strong>{' '}
+          {new Date(audit.date_of_request).toString()}
+        </Text>
+        <Text>
+          Accessibility: {audit.audit_result.accessibility.score * 100}
+        </Text>
+        <Text>
+          Best Practices: {audit.audit_result['best-practices'].score * 100}
+        </Text>
+        <Text>Performance: {audit.audit_result.performance.score * 100}</Text>
+        <Text>SEO: {audit.audit_result.seo.score * 100}</Text>
+      </Container>
+    </>
   )
 }
 

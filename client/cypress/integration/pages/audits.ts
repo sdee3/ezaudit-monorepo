89afcfecd5f3c6557ef508f66c76784254cb5072
Stefan <stefanjankovic921@gmail.com>
2022-01-26 20:29:06 +0100

    EA-44 Simulate API calls

--- client/cypress/integration/pages/audits.ts
+++ client/cypress/integration/pages/audits.ts
@@ -4,7 +4,13 @@ beforeEach('load dashboard with audits', () => {
   cy.visit('/dashboard/audits')
 
   cy.fixture('audits.json').then(data => (audits = data.message))
-  cy.intercept('/api/audits', { audits })
+  cy.intercept('/api/audits', req => {
+    req.reply({
+      message: audits,
+    })
+  })
+
+  cy.task('clearNock')
 })
 
 it('renders a table with audits', () => {
@@ -13,6 +19,17 @@ it('renders a table with audits', () => {
 
 it('opens an audit table row to view the audit in a separate page', () => {
   const auditId = audits[0].id
+
+  cy.task('nock', {
+    hostname: 'http://localhost:8000',
+    method: 'GET',
+    path: `/api/audits/${auditId}`,
+    statusCode: 200,
+    body: {
+      message: audits[0],
+    },
+  })
+
   cy.dataCy(`auditTableRow${auditId}`).click()
   cy.location('pathname').should('equal', `/dashboard/audits/${auditId}`)
 })

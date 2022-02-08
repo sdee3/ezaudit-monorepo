ebbdca92ea3bab8ffb7dddafb73ef9ca74c49108
Stefan <stefd996@gmail.com>
2022-02-08 20:46:32 +0100

    Removed service worker

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,19 +1,10 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
-import React, { useEffect } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { theme } from '../utils'
 import { Layout } from '../components'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', async () => {
-        await navigator.serviceWorker.register('/sw.js')
-      })
-    }
-  }, [])
-
   return (
     <ChakraProvider theme={theme}>
       <Layout>

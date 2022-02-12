59640fd05a5a88b8430f21a2db8144cbd2d63df8
Stefan <stefan.dokic@saboit.de>
2022-02-12 17:03:21 +0100

    SW fix?

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,22 @@
 // @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
 
 /**
  * @type {import('next').NextConfig}
+ * @type {import('next-pwa')}
  **/
-const nextConfig = {
+
+const nextConfig = withPWA({
   env: {
     API_URL: process.env.API_URL,
   },
   experimental: {
     styledComponents: true,
   },
-}
+  pwa: {
+    dest: 'public',
+  },
+})
 
 module.exports = nextConfig

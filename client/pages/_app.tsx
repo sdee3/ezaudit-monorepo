96194cd46fb3628202e58198024d79278ec9e9e3
Stefan <stefan.dokic@saboit.de>
2022-02-26 22:30:32 +0100

    EA-28 Cookie consent banner

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
 import { ChakraProvider } from '@chakra-ui/react'
 import { CookiesProvider } from 'react-cookie'
+import CookieConsent from 'react-cookie-consent'
+import Link from 'next/link'
 
-import { theme } from '../utils'
+import { ROUTES, theme } from '../utils'
 import { Layout, useAuthContext } from '../components'
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -18,6 +20,22 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Layout>
             <Component {...pageProps} />
           </Layout>
+
+          <CookieConsent
+            style={{ backgroundColor: theme.colors.primaryDark }}
+            buttonStyle={{
+              backgroundColor: theme.colors.white,
+              borderRadius: 4,
+              color: theme.colors.primaryDark,
+            }}
+          >
+            By continuing to use this site, you agree to our usage of cookies
+            for enhancing the user experience. See{' '}
+            <u>
+              <Link href={ROUTES.termsAndConditions}>Terms and Conditions</Link>
+            </u>{' '}
+            for more information.
+          </CookieConsent>
         </AuthContext.Provider>
       </CookiesProvider>
     </ChakraProvider>
